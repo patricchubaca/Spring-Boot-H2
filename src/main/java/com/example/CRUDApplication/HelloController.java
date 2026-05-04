98cc1def81bc954bbde2dd025e9e698e1db48609
patricchubaca <patric_job@outlook.com>
2026-05-04 16:16:46 -0300

    Feat :: CRUD

--- src/main/java/com/example/CRUDApplication/HelloController.java
+++ src/main/java/com/example/CRUDApplication/HelloController.java
@@ -1,13 +1,21 @@
-package com.example.CRUDApplication.controllers;
+package com.example.CRUDApplication;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+
+import java.util.List;
 
 @RestController
 public class HelloController {
 
     @GetMapping("/")
-    public String helloWorld() {
-        return "Hello World Spring";
+   public List<Livro> helloWorld() {
+        // Criando uma lista de livros para retornar como JSON
+        return List.of(
+            new Livro(1L, "O Senhor dos Anéis", "J.R.R. Tolkien"),
+            new Livro(2L, "Dom Casmurro", "Machado de Assis"),
+            new Livro(3L, "O Hobbit", "J.R.R. Tolkien")
+        );
     }
 }
---
+++ src/main/java/com/example/CRUDApplication/Livro.java
@@ -0,0 +1,4 @@
+package com.example.CRUDApplication;
+
+public record Livro(Long id, String titulo, String autor) {
+}
