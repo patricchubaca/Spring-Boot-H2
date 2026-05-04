170af1e7939a196b2324b46ced870f7f004b11e7
patricchubaca <patric_job@outlook.com>
2026-05-04 19:25:46 -0300

    Feat GET :: Get by ID

--- src/main/java/com/example/CRUDApplication/HelloController.java
+++ src/main/java/com/example/CRUDApplication/HelloController.java
@@ -11,6 +11,7 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
 @RestController
 public class HelloController {
@@ -50,6 +51,16 @@ public class HelloController {
             return new ResponseEntity<>(bookList, HttpStatus.OK);
         } catch(Exception ex) {
             return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
+
+    @GetMapping("/getBookById/{id}")
+    public ResponseEntity<Book> getBookById(@PathVariable Long id) {
+        Optional<Book> bookObj = bookRepository.findById(id);
+        if (bookObj.isPresent()) {
+            return new ResponseEntity<>(bookObj.get(), HttpStatus.OK);
+        } else {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
         }
     }
 }
