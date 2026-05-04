f963e7ab0de4641cbb202cce32279fcd203484ce
patricchubaca <patric_job@outlook.com>
2026-05-04 18:33:35 -0300

    Feat Create Book :: Model | Repository

--- src/main/java/com/example/CRUDApplication/HelloController.java
+++ src/main/java/com/example/CRUDApplication/HelloController.java
@@ -3,20 +3,36 @@ package com.example.CRUDApplication;
 import com.example.CRUDApplication.model.Book;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
-
+import com.example.CRUDApplication.repository.BookRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
 @RestController
 public class HelloController {
 
+    @Autowired
+    BookRepository bookRepository;
+
     @GetMapping("/")
-   public List<Book> helloWorld() {
+    public List<Book> helloWorld() {
         // Criando uma lista de livros para retornar como JSON
         return List.of(
-            new Book(1L, "O Senhor dos Anéis", "J.R.R. Tolkien"),
-            new Book(2L, "Dom Casmurro", "Machado de Assis"),
-            new Book(3L, "O Hobbit", "J.R.R. Tolkien")
-        );
+                new Book(1L, "O Senhor dos Anéis", "J.R.R. Tolkien"),
+                new Book(2L, "Dom Casmurro", "Machado de Assis"),
+                new Book(3L, "O Hobbit", "J.R.R. Tolkien"));
+    }
+
+    @PostMapping("/addBook")
+    public ResponseEntity<Book> addBook(@RequestBody Book book) {
+        try {
+            Book bookObj = bookRepository.save(book);
+            return new ResponseEntity<>(bookObj, HttpStatus.CREATED);
+        } catch (Exception e) {
+            return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
+        }
     }
 }
